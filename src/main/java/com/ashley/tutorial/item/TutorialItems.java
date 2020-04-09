4cbcf467c2eb242a315f7264b964d4817e8f5077
Ashley1227 <ashley.rued1227@gmail.com>
2020-04-09 14:41:12 -0400

    Recipes tutorial

---
+++ src/main/java/com/ashley/tutorial/item/TutorialItems.java
@@ -0,0 +1,18 @@
+package com.ashley.tutorial.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+
+public class TutorialItems {
+    public static final Item FLOUR = new Item(new Item.Settings().group(TutorialItemGroups.TUTORIAL_ITEMS).maxCount(64).rarity(Rarity.COMMON));
+
+    public static final Item MORTAR_AND_PESTLE = new MortarAndPestleItem(new Item.Settings().group(TutorialItemGroups.TUTORIAL_ITEMS).maxCount(1).rarity(Rarity.COMMON).recipeRemainder(Items.BOWL));
+
+    public static void register() {
+        Registry.register(Registry.ITEM, new Identifier("tutorial", "flour"), FLOUR);
+        Registry.register(Registry.ITEM, new Identifier("tutorial", "mortar_and_pestle"), MORTAR_AND_PESTLE);
+    }
+}

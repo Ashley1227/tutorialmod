8dd8a2edc211310271ae6ebc62ab9fd8db0e3964
Ashley1227 <ashley.rued1227@gmail.com>
2020-04-15 11:29:15 -0400

    Blocks Tutorial

--- src/main/java/com/ashley/tutorial/block/TutorialBlocks.java
+++ src/main/java/com/ashley/tutorial/block/TutorialBlocks.java
@@ -1,20 +1,18 @@
 package com.ashley.tutorial.block;
 
-import com.ashley.tutorial.item.MortarAndPestleItem;
 import com.ashley.tutorial.item.TutorialItemGroups;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class TutorialBlocks {
 
-    public static final Block BLOCK_OF_FLOUR = new Block(FabricBlockSettings.of(Material.SAND).build());
+    public static final Block BLOCK_OF_FLOUR = new Block(FabricBlockSettings.of(Material.SAND).sounds(BlockSoundGroup.SAND).build());
 
     public static void register() {
         Registry.register(Registry.BLOCK, new Identifier("tutorial","block_of_flour"), BLOCK_OF_FLOUR);
